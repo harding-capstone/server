2ae253c9106f87b4d773df9a5f17cc0aed2163b6
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-04-16 10:53:05 -0500

    Export game server

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.shepherdjerred.capstone.server {
   requires static lombok;
-
   requires com.shepherdjerred.capstone.logic;
   requires com.shepherdjerred.capstone.common;
   requires com.shepherdjerred.capstone.events;
@@ -10,4 +9,6 @@ module com.shepherdjerred.capstone.server {
   requires org.apache.logging.log4j;
   requires gson;
   requires gson.extras;
+
+  exports com.shepherdjerred.capstone.server;
 }
